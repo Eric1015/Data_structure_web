b74c1473dcae43dc5385307098153d4196087b6a
love1 <love1dbsb77@hotmail.com>
2018-04-16 12:03:07 -0700

    Complete a stack with drawing to push and pop, need to fix some small bugs

--- js/stackCanvas.js
+++ js/stackCanvas.js
@@ -9,15 +9,23 @@ $(document).ready(function(){
 
     var currNum = 0;
     const MAX_SIZE = 10;
-    $("#stack-pop").onclick(function(){
+    $("#stack-pop").click(function(){
         if (currNum > 0){
-
+            var currX = 30;
+            var currY = (MAX_SIZE - currNum) * 34 + 34;
+            ctx.fillStyle="#fff";
+            ctx.fillRect(currX, currY, 240, 34);
+            currNum--;
         }
     });
 
-    $("#stack-push").onclick(function(){
+    $("#stack-push").click(function(){
         if (currNum < MAX_SIZE){
-            
+            var currX = 30;
+            var currY = (MAX_SIZE - currNum - 1) * 34 + 34;
+            ctx.fillStyle="#ff0000";
+            ctx.fillRect(currX, currY, 240, 34);
+            currNum++;
         }
     });
 });
