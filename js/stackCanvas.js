bb45fef2f556d575a995a390baa6b1bc45d81b70
love1 <love1dbsb77@hotmail.com>
2018-04-16 14:21:10 -0700

    Added the drawRoundRect function and work properly, need to work on pop

--- js/stackCanvas.js
+++ js/stackCanvas.js
@@ -22,16 +22,31 @@ $(document).ready(function(){
     $("#stack-push").click(function(){
         if (currNum < MAX_SIZE){
             var currX = 30;
-            var currY = (MAX_SIZE - currNum - 1) * 34 + 34;
+            var currY = (MAX_SIZE - currNum - 1) * 34 + 30;
             ctx.fillStyle="#ff0000";
             ctx.strokeStyle="#000";
-            ctx.fillRect(currX, currY, 240, 34);
-            ctx.strokeRect(currX, currY, 240, 34);
+            drawRoundRect(currX, currY);
             ctx.stroke();
             currNum++;
         }
     });
 
+    function drawRoundRect(x, y){
+        var r = 10;
+        ctx.beginPath();
+        ctx.arc(x+4+r, y+2+r, r, 1*Math.PI, 1.5*Math.PI, false);
+        ctx.lineTo(240-r-4, y+2);
+        ctx.arc(x+240-r-4, y+r+2, r, 1.5*Math.PI, 0, false);
+        ctx.lineTo(x+240-4, y+34-r-2);
+        ctx.arc(x+240-r-4, y+34-r-2, r, 0, 0.5*Math.PI, false);
+        ctx.lineTo(x+r+4, y+34-2);
+        ctx.arc(x+r+4, y+34-r-2, r, 0.5*Math.PI, 1*Math.PI, false);
+        ctx.lineTo(x+4, y+r+2);
+        ctx.closePath();
+        ctx.fillStyle="red";
+        ctx.fill();
+    }
+    
     function fadeIn(){
 
     }
