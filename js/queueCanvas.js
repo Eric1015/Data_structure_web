218bafdfde60418ad403270381c2e359eb5d4e9c
love1 <love1dbsb77@hotmail.com>
2018-04-17 14:04:38 -0700

    queue pop implementation completed but need animation to correctly show the process

--- js/queueCanvas.js
+++ js/queueCanvas.js
@@ -13,16 +13,19 @@ $(document).ready(function(){
         if (currNum < MAX_SIZE){
             currX = currNum*34+30;
             currY = 50;
-            drawRoundRect(currX, currY);
+            drawRoundRect(currX, currY, currNum);
             currNum++;
         }
     });
 
     $("#queue-pop").click(function(){
-
+        if (currNum > 0){
+            redraw();
+            currNum--;
+        }
     });
 
-    function drawRoundRect(x, y){
+    function drawRoundRect(x, y, index){
         var r = 10;
         ctx.beginPath();
         ctx.arc(x + 4 + r, y + 2 + r, r, 1 * Math.PI, 1.5 * Math.PI, false);
@@ -38,13 +41,33 @@ $(document).ready(function(){
         ctx.fill();
         ctx.font = "12px Arial";
         ctx.fillStyle = "#000";
-        if (currNum >= 9)
-            ctx.fillText(currNum + 1, x + 12, y + 50);
+        if (index >= 9)
+            ctx.fillText(index + 1, x + 12, y + 50);
         else
-            ctx.fillText(currNum + 1, x + 15, y + 50);
+            ctx.fillText(index + 1, x + 15, y + 50);
     }
 
-    function redraw(x, y){
+    function redraw(){
+        clearRoundRect(30, 50);
+        for (i = 0; i < currNum - 1; i++){
+            clearRoundRect(30+(i+1)*34, 50);
+            drawRoundRect(30+i*34, 50, i);
+        }
+    }
 
+    function clearRoundRect(x, y) {
+        var r = 10;
+        ctx.beginPath();
+        ctx.arc(x + 3 + r, y + 1 + r, r, 1 * Math.PI, 1.5 * Math.PI, false);
+        ctx.lineTo(x+34 - r - 3, y + 1);
+        ctx.arc(x + 34 - r - 3, y + r + 1, r, 1.5 * Math.PI, 0, false);
+        ctx.lineTo(x + 34 - 3, y + 100 - r - 1);
+        ctx.arc(x + 34 - r - 3, y + 100 - r - 1, r, 0, 0.5 * Math.PI, false);
+        ctx.lineTo(x + r + 3, y + 100 - 1);
+        ctx.arc(x + r + 3, y + 100 - r - 1, r, 0.5 * Math.PI, 1 * Math.PI, false);
+        ctx.lineTo(x + 3, y + r + 1);
+        ctx.closePath();
+        ctx.fillStyle = "#fff";
+        ctx.fill();
     }
 });
