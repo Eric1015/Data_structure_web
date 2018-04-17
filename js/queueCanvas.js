8ccad1e9fc7a1fbb7b79b8d5b8146f1db180aaad
love1 <love1dbsb77@hotmail.com>
2018-04-17 13:40:58 -0700

    Push function completed for queue

--- js/queueCanvas.js
+++ js/queueCanvas.js
@@ -7,8 +7,15 @@ $(document).ready(function(){
     ctx.lineTo(370, 150);
     ctx.stroke();
 
+    var currNum = 0;
+    const MAX_SIZE = 10;
     $("#queue-push").click(function(){
-
+        if (currNum < MAX_SIZE){
+            currX = currNum*34+30;
+            currY = 50;
+            drawRoundRect(currX, currY);
+            currNum++;
+        }
     });
 
     $("#queue-pop").click(function(){
@@ -16,10 +23,28 @@ $(document).ready(function(){
     });
 
     function drawRoundRect(x, y){
-
+        var r = 10;
+        ctx.beginPath();
+        ctx.arc(x + 4 + r, y + 2 + r, r, 1 * Math.PI, 1.5 * Math.PI, false);
+        ctx.lineTo(x+34 - r - 4, y + 2);
+        ctx.arc(x + 34 - r - 4, y + r + 2, r, 1.5 * Math.PI, 0, false);
+        ctx.lineTo(x + 34 - 4, y + 100 - r - 2);
+        ctx.arc(x + 34 - r - 4, y + 100 - r - 2, r, 0, 0.5 * Math.PI, false);
+        ctx.lineTo(x + r + 4, y + 100 - 2);
+        ctx.arc(x + r + 4, y + 100 - r - 2, r, 0.5 * Math.PI, 1 * Math.PI, false);
+        ctx.lineTo(x + 4, y + r + 2);
+        ctx.closePath();
+        ctx.fillStyle = "#ff8800";
+        ctx.fill();
+        ctx.font = "12px Arial";
+        ctx.fillStyle = "#000";
+        if (currNum >= 9)
+            ctx.fillText(currNum + 1, x + 12, y + 50);
+        else
+            ctx.fillText(currNum + 1, x + 15, y + 50);
     }
 
     function redraw(x, y){
-        
+
     }
 });
