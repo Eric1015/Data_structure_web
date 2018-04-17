f84d651a446be64d49dde17f4bbf26fe24f938b3
love1 <love1dbsb77@hotmail.com>
2018-04-17 13:30:12 -0700

    Added the queueCanvas.js file

---
+++ js/queueCanvas.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    var canvas = document.getElementById("queue-canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.moveTo(30, 50);
+    ctx.lineTo(370, 50);
+    ctx.moveTo(30, 150);
+    ctx.lineTo(370, 150);
+    ctx.stroke();
+
+    $("#queue-push").click(function(){
+
+    });
+
+    $("#queue-pop").click(function(){
+
+    });
+
+    function drawRoundRect(x, y){
+
+    }
+
+    function redraw(x, y){
+        
+    }
+});
