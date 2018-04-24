0e6974979b126f53ccbe802761afb6dedb9669f2
love1 <love1dbsb77@hotmail.com>
2018-04-23 22:58:33 -0700

    Added animation in stack push and pop as well

--- js/stackCanvas.js
+++ js/stackCanvas.js
@@ -9,27 +9,41 @@ $(document).ready(function () {
     ctx.font = "20px Arial";
     ctx.fillText("Buttom", 180, 370);
 
+    var beforePush = 0;
+    var beforePop = 0;
     var currNum = 0;
+    var counter = 0;
     const MAX_SIZE = 10;
+    var speed = 5;
+    var i = 0;
+    var requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
+
     $("#stack-pop").click(function () {
         if (currNum > 0) {
-            var currX = 30;
-            var currY = (MAX_SIZE - currNum) * 34 + 30;
-            drawRoundRect(currX, currY, 3, 1, 140, 34, 10, "#fff", "#fff");
+            counter = 0;
+            beforePop = currNum - 1;
+            i = currNum;
+            document.getElementById("stack-pop").disabled = true;
+            requestAnimationFrame(popElement);
             currNum--;
         }
     });
 
     $("#stack-push").click(function () {
         if (currNum < MAX_SIZE) {
-            var currX = 30;
-            var currY = (MAX_SIZE - currNum - 1) * 34 + 30;
-            drawRoundRect(currX, currY, 4, 2, 140, 34, 10, "#ff88ff");
+            counter = 0;
+            i = MAX_SIZE;
+            beforePush = currNum;
+            document.getElementById("stack-push").disabled = true;
+            requestAnimationFrame(pushElement);
             currNum++;
         }
     });
 
-    function drawRoundRect(x, y, mx, my, width, height, r, color, fontColor = "#000", font = "12px Arial") {
+    function drawRoundRect(x, y, index, mx, my, width, height, r, color, fontColor = "#000", font = "12px Arial") {
         ctx.beginPath();
         ctx.arc(x + mx + r, y + my + r, r, 1 * Math.PI, 1.5 * Math.PI, false);
         ctx.lineTo(width - r - mx, y + my);
@@ -44,17 +58,41 @@ $(document).ready(function () {
         ctx.fill();
         ctx.font = font;
         ctx.fillStyle = fontColor;
-        if (currNum >= 9)
-            ctx.fillText(currNum + 1, x + width / 2 - 5, y + height / 2 + 2);
+        if (index >= 9)
+            ctx.fillText(index + 1, x + width / 2 - 5, y + height / 2 + 2);
         else
-            ctx.fillText(currNum + 1, x + width / 2 - 2, y + height / 2 + 2);
+            ctx.fillText(index + 1, x + width / 2 - 2, y + height / 2 + 2);
     }
 
-    function fadeIn() {
-
+    function pushElement() {
+        if (++counter % speed == 0) {
+            if (i > beforePush) {
+                drawRoundRect(30, (MAX_SIZE - i - 1) * 34 + 30, beforePush, 3, 1, 140, 34, 10, "#fff", "#fff");
+                drawRoundRect(30, (MAX_SIZE - i) * 34 + 30, beforePush, 4, 2, 140, 34, 10, "#ff88ff");
+                i--;
+                requestAnimationFrame(pushElement);
+            }
+            else
+                document.getElementById("stack-push").disabled = false;
+        }
+        else
+            requestAnimationFrame(pushElement);
     }
 
-    function fadeOut() {
-
+    function popElement() {
+        if (++counter % speed == 0){
+            if (i < MAX_SIZE){
+                drawRoundRect(30, (MAX_SIZE - i)* 34 + 30, beforePop, 3, 1, 140, 34, 10, "#fff", "#fff");
+                drawRoundRect(30, (MAX_SIZE - i - 1) * 34 + 30, beforePop, 4, 2, 140, 34, 10, "#ff88ff");
+                i++;
+                requestAnimationFrame(popElement);
+            }
+            else{
+                drawRoundRect(30, 30, beforePop, 3, 1, 140, 34, 10, "#fff", "#fff");
+                document.getElementById("stack-pop").disabled = false;
+            }
+        }
+        else 
+            requestAnimationFrame(popElement);
     }
 });
